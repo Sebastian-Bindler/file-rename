fdf3d266ddf60b58da0c1ba608d581b4d9c7a909
Sebastian Bindler <31371348+Sebastian-Bindler@users.noreply.github.com>
2022-03-03 10:35:59 +0100

    png-rename

---
+++ main.py
@@ -0,0 +1,39 @@
+import os.path
+import glob
+from PIL import Image 
+
+trimr = 10
+triml = 10
+
+filenameExtension = "png"
+
+targetPattern = r"input/*.{}".format(filenameExtension)
+
+if __name__ == "__main__":
+    
+    daten = glob.glob(targetPattern)
+    datenIndex = len(daten)
+    
+    for i in range (0 , datenIndex):
+            
+        filename = daten[i]
+        img = Image.open(filename)
+        
+        string = str(os.path.basename(filename))
+        l = len(string) - triml - 4
+        string = string[trimr:l]
+        print(string)
+        path = "output/{}.{}".format(string, filenameExtension)
+        
+        img.save(path)
+        
+        
+            
+        
+        
+        
+        
+        
+        
+
+        
